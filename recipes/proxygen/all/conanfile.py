5f5d39e4ce6ff1aced3612f052faa6d8192c888a
Xianda Ke <kexianda@gmail.com>
2022-11-22 14:09:18 +0800

    fix proxygen

--- recipes/proxygen/all/conanfile.py
+++ recipes/proxygen/all/conanfile.py
@@ -1,5 +1,7 @@
 from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
 from conan.tools import files
+from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.files import apply_conandata_patches, export_conandata_patches, copy
 from conan.tools.microsoft import is_msvc
